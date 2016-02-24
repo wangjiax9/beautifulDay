41f02a0b1a5fa17607bda4ac84958c459e21380c
wangjiax9 <6215048wjl@163.com>
2016-02-24 15:44:33 +0800

    提交webpack成功案例

---
+++ js/app.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Router from 'react-router';
+import { DefaultRoute, Link, Route, RouteHandler } from 'react-router';
+import HelloHandler from './hello.js';
+let App = React.createClass({
+  render() {
+    return (
+      <div className="nav">
+        <Link to="app" className="homelink">webpack  </Link>
+        <Link to="hello" className="hellolink">  Say Hello react</Link>
+        {/* this is the importTant part */}
+        <RouteHandler/>
+      </div>
+    );
+  }
+});
+let routes = (
+  <Route name="app" path="/" handler={App}>
+    <Route name="hello" path="/hello" handler={HelloHandler}/>
+  </Route>
+);
+Router.run(routes, function (Handler) {
+  React.render(<Handler />, document.body);
+});
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+module.exports = {
+	entry: [
+		'webpack/hot/only-dev-server',
+		"./js/app.js"
+	],
+	output: {
+		path: './build',
+		filename: "bundle.js"
+	},
+	module: {
+		loaders: [{
+			test: /\.js?$/,
+			loaders: ['react-hot', 'babel'],
+			exclude: /node_modules/
+		}, {
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader'
+		}, {
+			test: /\.css$/,
+			loader: "style!css"
+		}]
+	},
+	resolve: {
+		extensions: ['', '.js', '.json']
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(), //热替换插件
+		new webpack.NoErrorsPlugin()	//防止报错插件
+	]
+};
